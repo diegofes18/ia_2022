f57c333fe8ec26b35a12b0fb6d71f1ee87bd51b2
Diego <bermejodiego1@gmail.com>
2022-10-18 12:59:41 +0200

    cosas

--- monedes/agent.py
+++ monedes/agent.py
@@ -7,8 +7,33 @@ Solució:
 """
 
 from ia_2022 import agent, entorn
+from monedes.entorn import ClauPercepcio, AccionsMoneda
+from queue import PriorityQueue
+from monedes.joc import Moneda
 
 SOLUCIO = " XXXC"
+
+class Estat():
+
+
+    def __init__(self, pare=None):
+        self.__pare = pare
+        self.__monedas = Moneda
+
+
+        def get_pare(self):
+            return self.__pare
+        def set_pare(self, value):
+            self.__pare = value
+
+        def genera_fill(self) -> dict:
+            # numero entero
+            costs = dict()
+            for act in AccionsMoneda:
+                estatf_i = Estat()
+                cost = act.value
+                estatf_i = Moneda._aplica(act)
+
 
 
 class AgentMoneda(agent.Agent):
@@ -21,7 +46,53 @@ class AgentMoneda(agent.Agent):
     def pinta(self, display):
         print(self._posicio_pintar)
 
+    def _cerca(self, estat: Estat):
+        self.__oberts = PriorityQueue()
+        self.__tancats = set()
+
+        self.__oberts.put(3, estat)
+        actual = None
+        while not self.__oberts.empty():
+            actual = self.__oberts.get()
+            #self.__oberts = self.__oberts[1:]
+
+            if actual in self.__tancats:
+                continue
+
+            #if not actual.es_segur():
+            #    self.__tancats.add(actual)
+            #    continue
+
+            estats_fills = actual.genera_fill()
+
+            if actual.es_meta():
+                break
+
+            for estat_f in estats_fills:
+                pass
+                #self.__oberts.append(estat_f)
+
+            self.__tancats.add(actual)
+        if actual is None:
+            raise ValueError("Error impossible")
+
+        if actual.es_meta():
+            accions = []
+            iterador = actual
+
+            while iterador.pare is not None:
+                pare, accio = iterador.pare
+
+                accions.append(accio)
+                iterador = pare
+            self.__accions = accions
+            return True
+
+
+    def calculate_f(self, estatf:Estat, estatp:Estat):
+        pass
+
     def actua(
-        self, percep: entorn.Percepcio
+            self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
         pass
